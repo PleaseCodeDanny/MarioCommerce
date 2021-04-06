bd470a2c9a0d5658ceb5b82f5076565d91782978
DanielTrevino <dtrevino@terpmail.umd.edu>
2021-04-05 20:03:39 -0400

    First commit of backend.

---
+++ backend/base/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    # name =
+    # # image =
+    # brand =
+    # category =
+    # description =
+    # rating =
+    # numReviews =
+    # price =
+    # countInStock =
+    # createdAt =
+    # _id =
+
+
