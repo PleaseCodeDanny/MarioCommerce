a672ea50f2c7c666c60df75a30665faed50c4693
PleaseCodeDanny <Guiltygear3>
2021-04-12 21:48:10 -0400

    Added and tested update user profile endpoint.

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', user_views.getUsers, name='users'),
     path('login/', user_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', user_views.registerUser, name='register'),
+    path('profile/update/', user_views.updateUserProfile, name='user-profile-update'),
     path('profile/', user_views.getUserProfile, name='user-profile'),
-
 
 ]
