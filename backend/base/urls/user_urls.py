26e437374e8e16c9100bb5c494a57aa9f76472a7
PleaseCodeDanny <Guiltygear3>
2021-04-27 16:27:27 -0400

    User edit and user list admin screens. (backend)

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 from base.views import user_views
 
 urlpatterns = [
-    path('', user_views.getUsers, name='users'),
+
     path('login/', user_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', user_views.registerUser, name='register'),
     path('profile/update/', user_views.updateUserProfile, name='user-profile-update'),
     path('profile/', user_views.getUserProfile, name='user-profile'),
+
+    path('', user_views.getUsers, name='users'),
+
+    path('<str:pk>/', user_views.getUserById, name='user'),
+    path('update/<str:pk>', user_views.updateUser, name='user-update'),
+    path('delete/<str:pk>', user_views.deleteUser, name='user-delete'),
+
 
 ]
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -59,6 +60,48 @@ def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUserById(request, pk):
+    try:
+        user = User.objects.get(id=pk)
+        serializer = UserSerializer(user, many=False)
+        return Response(serializer.data)
+    except ObjectDoesNotExist:
+        return Response('User does not exist')
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUser(request, pk):
+    user = User.objects.get(id=pk)
+
+    data = request.data
+
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    user.is_staff = data['isAdmin']
+
+    user.save()
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteUser(request, pk):
+    '''
+    Notes: Python stores the default user object ids as id. so that's what we must use.
+    Since we actually defined our order,product, etc. and created an _id for our use, we use _id in that case.
+    Notice that user id is removed from the user object once it is deleted. Everything else in the user seems to be intact...
+    '''
+    userForDeletion = User.objects.get(id=pk)
+    userForDeletion_id = userForDeletion.id
+    userForDeletion.delete()
+    return Response(f'User {userForDeletion.username} with id: {userForDeletion_id} was deleted')
 
 
 @api_view(['POST'])
